127fbe2a5082bdfde32b0cc5ee7fa0afcee9da89
Warit Santaputra <waritsan@gmail.com>
2018-04-04 11:19:55 +0700

    Add hello hapi

---
+++ hello_hapi.js
@@ -0,0 +1,21 @@
+const Hapi = require('hapi');
+
+(async () => {
+    try {
+      const server = new Hapi.Server({
+        host: 'localhost',
+        port: Number(process.argv[2] || 8080)
+      });
+      server.route({
+        path: '/',
+        method: 'GET',
+        handler: function (request, h) {
+          return "Hello hapi";
+        }
+      });
+      server.start();
+      console.log('Server running at: ', server.info.uri);
+    } catch (e) {
+      console.log(e);
+    }
+})();
